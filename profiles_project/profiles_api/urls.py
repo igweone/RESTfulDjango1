c946efdf05b6d6ace1d482b3ef50c40800a17f38
Emeka Osigwe <emeka@intelligenti.io>
2019-03-28 13:05:54 +0100

    added Http CRUD methods

--- profiles_project/profiles_api/urls.py
+++ profiles_project/profiles_api/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from .views import HelloApiView
 
 urlpatterns = [
-    url(r'^hello/', HelloApiView.as_view()),
+    url(r'^hello', HelloApiView.as_view()),
    
 ]
--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.views import Response
+from rest_framework import status
+
+from .serializers import HelloSerializer
 
 
 class HelloApiView(APIView):
     """Test API View"""
+    serializer_class = HelloSerializer
+
     def get(self, request, format=None):
         """Returns a list of APIView Features"""
         an_apiview = [
@@ -13,4 +18,32 @@ class HelloApiView(APIView):
             'i like it'
         ]
         return Response({'message':'hello', 'an_apiview':an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name """
+        serializer = HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = "Hello {0}".format(name)
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self, request, pk=None):
+        """Handles updating a specific object"""
+
+        return Response({'method':'put'})
+
+    
+    def patch(self, request, pk=None):
+        """Patch request only updates fields provided in the request"""
+
+        return Response({'method':'patch'})
+
+    
+    def delete(self, request, pk=None):
+        """Deletes an object"""
+
+        return Response({'method':'delete'})
 
