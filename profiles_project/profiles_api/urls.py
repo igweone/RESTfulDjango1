bf283d45bc0fc3810d1ecb101e9cf41bb8e9e688
Emeka Osigwe <emeka@intelligenti.io>
2019-03-28 12:19:09 +0100

    hello world using APIView

---
+++ profiles_project/profiles_api/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from .views import HelloApiView
+
+urlpatterns = [
+    url(r'^hello/', HelloApiView.as_view()),
+   
+]
--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.views import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test API View"""
+    def get(self, request, format=None):
+        """Returns a list of APIView Features"""
+        an_apiview = [
+            'Uses Http methods as functions(get,post,patch,put, delte)',
+            'it is cool',
+            'i like it'
+        ]
+        return Response({'message':'hello', 'an_apiview':an_apiview})
+
